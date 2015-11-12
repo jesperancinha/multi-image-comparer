0b1d6c93c3a40820cc3a94daa7f6f4b5a7bc3ff4
Joao Esperancinha <jofisaes@hotmail.com>
2015-11-11 16:45:05 -0800

    SZ Better build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
+echo ---- Creating system variables...
 export GOROOT=/usr/lib/go
-export GOPATH=/opt/go/multi-image-comparer/lib-multi-comparer:/opt/go/multi-image-comparer/mic-example-images:/opt/go/multi-image-comparer/pearson-correlation-coefficient
+export GOPATH=/opt/go/multi-image-comparer/lib
 export PATH=$PATH:$GOPATH:$GOROOT/bin
-export GOBIN=/opt/go/multi-image-comparer/lib-multi-comparer/bin:/opt/go/multi-image-comparer/mic-example-images/bin:/opt/go/multi-image-comparer/pearson-correlation-coefficient/bin
+export GOBIN=/opt/go/multi-image-comparer/lib-multi-comparer/bin
 go env
+
+echo ---- Loading commong libraries
+go get "github.com/stretchr/testify/assert"
+echo -------- Loaded github.com/stretchr/testify/assert
+go get "github.com/stretchr/objx"
+echo -------- Loaded github.com/stretchr/objx
+
+echo ---- Creating pkg builds
+export GOPATH=/opt/go/multi-image-comparer/lib-multi-comparer
+cd lib-multi-comparer
+go build -x src/github.com/steelzack/lib-multi-comparer/multicomparer/image-utils.go
+go install -v -gcflags "-N -l" ./...
+cd ..
